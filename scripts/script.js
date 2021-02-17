4e35597e10cfa53c1c0938e64fda6212cbf91e66
sultonbayevogabek <70335611+sultonbayevogabek@users.noreply.github.com>
2021-02-17 14:40:21 +0500

    video-play.js update

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,17 @@ import doctors from "./doctors";
 import videoPlay from "./video-play";
 
 document.addEventListener('DOMContentLoaded', () => {
+   const videosData = [
+      {
+         id: 1,
+         posterURL: 'img/gallery/poster.png',
+         videoURL: 'media/video.mp4',
+         videoTitle: 'Технологии будущего для вашего здоровья'
+      }
+   ];
+   videoPlay(videosData, '.video-gallery:first-child');
    customSelect();
    sliderSettings();
    siteNav();
    doctors();
-   videoPlay();
 })
--- scripts/video-play.js
+++ scripts/video-play.js
@@ -1,24 +1,50 @@
-import {selectElement} from "./selector-functions";
+import {selectAllElements, selectElement} from "./selector-functions";
 
-function videoPlay() {
-   // const videosData = [
-   //    {
-   //       id
-   //    }
-   // ]
-   const video = selectElement('video'),
-      playBtn = selectElement('.video-play'),
-      playBtnIcon = selectElement('.video-play img');
+function videoPlay(array, selector) {
 
-   playBtn.onclick = () => {
-      if (video.paused) {
-         video.play();
-         playBtnIcon.src = 'img/icons/pause.svg';
-      } else {
-         video.pause();
-         playBtnIcon.src = 'img/icons/play.svg';
-      }
-   }
+   const videosGallery = selectElement(selector);
+
+   array.forEach(({id, posterURL, videoTitle, videoURL}) => {
+      videosGallery.innerHTML += `
+         <div class="video-wrapper">
+            <div class="video">
+               <video poster=${posterURL}>
+                  <source src=${videoURL} type="video/mp4">
+               </video>
+               <button class="video-play">
+                  <img src="img/icons/play.svg" alt="">
+               </button>
+            </div>
+            <div class="video-gallery__text">
+              ${videoTitle}
+            </div>
+         </div>
+      `
+   })
+   const playButtons = selectAllElements('.video-play'),
+      allVideos = selectAllElements('video');
+
+   playButtons.forEach(btn => {
+      btn.addEventListener('click', e => {
+         allVideos.forEach(video => {
+            video.pause()
+            const pauseBtnIcon = video.nextElementSibling.querySelector('img');
+            pauseBtnIcon.src = 'img/icons/play.svg';
+         });
+         
+         const currentBtn = e.currentTarget,
+            currentBtnIcon = currentBtn.querySelector('img'),
+            currentVideo = currentBtn.previousElementSibling;
+
+         if (currentVideo.paused) {
+            currentVideo.play();
+            currentBtnIcon.src = 'img/icons/pause.svg';
+         } else {
+            currentVideo.pause();
+            currentBtnIcon.src = 'img/icons/play.svg';
+         }
+      })
+   })
 }
 
 export default videoPlay;
