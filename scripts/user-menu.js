7af68fa7a00eef7653b7fe1966870cb5db8d727a
sultonbayevogabek <70335611+sultonbayevogabek@users.noreply.github.com>
2021-02-27 01:14:07 +0500

    signed in html done

--- scripts/user-menu.js
+++ scripts/user-menu.js
@@ -1,31 +1,28 @@
-const elUserMenu = document.querySelector(".user__menu");
-const elModalUser = document.querySelector(".modal__user");
+const elUserMenu = document.querySelector('.user__menu');
+const elModalUser = document.querySelector('.modal__user');
 
 function clearClasses() {
-   elModalUser.classList.remove("fade-out", "show", "fade-in");
+   elModalUser.classList.remove('fade-out', 'show', 'fade-in');
 }
 
-elUserMenu.addEventListener("click", (evt) => {
-   if (evt.target.classList.contains("modal__content")) {
+elUserMenu.addEventListener('click', (evt) => {
+   if (evt.target.classList.contains('modal__content')) {
       return;
    }
-   if (!elModalUser.classList.contains("fade-in")) {
-      document.removeEventListener("animationend", clearClasses);
-      elModalUser.classList.add("fade-in", "show");
+   if (!elModalUser.classList.contains('fade-in')) {
+      document.removeEventListener('animationend', clearClasses);
+      elModalUser.classList.add('fade-in', 'show');
 
-      window.addEventListener("click", (evt) => {
-         if (!evt.target.classList.contains("modal__content") && !(evt.target.classList.contains("user__menu") || evt.target.classList.contains("user__menu__img") || evt.target.classList.contains("user__menu__text") || evt.target.classList.contains("icon__bottom") || evt.target.classList.contains("user__img__wrapper"))) {
-            elModalUser.classList.add("fade-out");
-            document.addEventListener("animationend", clearClasses);
+      window.addEventListener('click', (evt) => {
+         if (!evt.target.classList.contains('modal__content') && !(evt.target.classList.contains('user__menu') ||
+            evt.target.classList.contains('user__menu__img') || evt.target.classList.contains('user__menu__text') ||
+            evt.target.classList.contains('icon__bottom') || evt.target.classList.contains('user__img__wrapper'))) {
+            elModalUser.classList.add('fade-out');
+            document.addEventListener('animationend', clearClasses);
          }
       })
    } else {
-      elModalUser.classList.add("fade-out");
-      document.addEventListener("animationend", clearClasses);
-   }
-
-
-   if (elModalUser.classList.contains("fade-out")) {
-      clearClasses();
+      elModalUser.classList.add('fade-out');
+      document.addEventListener('animationend', clearClasses);
    }
 })
